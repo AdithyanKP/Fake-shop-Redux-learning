21817459e8e5dd4da38dae30a74fafada1b68904
Adithyan k P <adithyankp777@gmail.com>
2022-03-10 22:15:09 +0530

    redux started

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,13 @@
+import { ActionTypes } from "../constants/actionTypes";
+export const setProduct = (product) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: product,
+  };
+};
+export const selectedProduct = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCTS,
+    payload: product,
+  };
+};
